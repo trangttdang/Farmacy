bceded0858acf6bfe8cc7f53c2872183d0bc48ff
Trang Dang <trangttdang01@gmail.com>
2022-08-02 11:23:28 -0700

    Polished chat UI

---
+++ Farmacy/Views/RecipientChatCell.h
@@ -0,0 +1,16 @@
+//
+//  RecipientChatCell.h
+//  Farmacy
+//
+//  Created by Trang Dang on 8/2/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RecipientChatCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Farmacy/Views/SenderChatCell.h
@@ -0,0 +1,16 @@
+//
+//  SenderChatCell.h
+//  Farmacy
+//
+//  Created by Trang Dang on 8/2/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SenderChatCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
