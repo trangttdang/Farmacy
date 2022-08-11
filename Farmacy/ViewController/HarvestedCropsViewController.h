c6da558ea371e8f2604156681f44ae4854735dd6
Trang Dang <trangttdang01@gmail.com>
2022-08-11 13:08:03 -0700

    Implemented Segment Control to switch between planting and harvested views

---
+++ Farmacy/ViewController/HarvestedCropsViewController.h
@@ -0,0 +1,16 @@
+//
+//  HarvestedCropsViewController.h
+//  Farmacy
+//
+//  Created by Trang Dang on 8/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HarvestedCropsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Farmacy/ViewController/MyCropsViewController.h
+++ Farmacy/ViewController/MyCropsViewController.h
@@ -11,7 +11,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface MyCropsViewController : UIViewController;
+@interface MyCropsViewController : UIViewController
 @property (nonatomic) Crop *crop;
 
 @end
