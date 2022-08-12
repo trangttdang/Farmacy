68f2b57ece7fe804f881b07e9d3b9305ccacd9be
Trang Dang <trangttdang01@gmail.com>
2022-08-12 12:05:32 -0700

    Implemented crop image classification using Core ML model - MobileNetV2

---
+++ Farmacy/ViewController/CropDetectionViewController.h
@@ -0,0 +1,16 @@
+//
+//  CropDetectionViewController.h
+//  Farmacy
+//
+//  Created by Trang Dang on 8/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CropDetectionViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
